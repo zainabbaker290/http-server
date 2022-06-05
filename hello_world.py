3a90deb55b05ab945474aa8feadb274df683b121
zainabbaker290 <119340513@umail.ucc.ie>
2022-06-05 20:17:23 +0100

    first two endpoints

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,29 @@
 from flask import Flask
+import json
+from users import users
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
         return "Hello World!"
+
+@app.route("/all_users")
+def all_users():
+    json_dump_users = json.dumps(users)
+    json_object_users = json.loads(json_dump_users)
+    return json_object_users
+
+@app.route("/get_user/<user_id>")
+def get_user(user_id):
+    user_returned = None
+    json_dump_users = json.dumps(users)
+    json_object_users = json.loads(json_dump_users)
+
+    for key, value in json_object_users.items():
+        for v in value:
+            if v == user_id:
+                return key
 
 if __name__ == "__main__":
     app.run()

34be68a365a49925f72d403d3726aea581703af8
zainabbaker290 <119340513@umail.ucc.ie>
2022-06-07 22:07:13 +0100

    adding third end point, doesnt work

--- hello_world.py
+++ hello_world.py
@@ -23,7 +23,9 @@ def get_user(user_id):
 
 @app.route("/all_users")
 def all_users():
+    #comverts python object into json string
     json_dump_users = json.dumps(users)
+    #changes it to python dictionary
     json_object_users = json.loads(json_dump_users)
     return json_object_users
 
@@ -36,7 +38,18 @@ def get_user(user_id):
     for key, value in json_object_users.items():
         for v in value:
             if v == user_id:
-                return key
+                user_returned = value
+                result = user_returned[0] + " " + user_returned[1] + " " + user_returned[2]
+                return result
+
+@app.route("/add_user", methods=["POST"])
+def add_user(name, dob, user_id):
+    #does not work
+    json_dump_users = json.dumps(users)
+    json_object_users = json.loads(json_dump_users)
+    user_key = name.lower()
+    json_object_users[user_key] = [name, dob, user_id]
+    return json_object_users
 
 if __name__ == "__main__":
     app.run()

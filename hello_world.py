3d8fe9faaf56de19c892179c59ad70ff8a1e2fba
zainabbaker290 <119340513@umail.ucc.ie>
2022-06-08 19:22:26 +0100

    fixed with dumps

--- hello_world.py
+++ hello_world.py
@@ -13,7 +13,7 @@ def all_users():
     users_file = open("users.json", "r")
     #using json loads to read from a string 
     users_object = json.load(users_file)
-    return users_object
+    return json.dumps(users_object)
 
 @app.route("/users/<user_id>", methods=["GET"])
 def get_user(user_id):
@@ -21,12 +21,9 @@ def get_user(user_id):
     users_object = json.load(users_file)
 
     #using dictionary attributes to access key and values 
-    for key, value in users_object.items():
-        for v in value:
-            if v == user_id:
-                user_returned = value
-                result = key + ": " + user_returned[0] + " " + user_returned[1] + " " + user_returned[2]
-                return result
+    for key in users_object.keys():
+        if key == user_id:
+            return json.dumps(users_object[key])
 
 if __name__ == "__main__":
     app.run()
--- users.py
+++
@@ -1,6 +0,0 @@
-import json
-
-users = {"tim": ["Tim", "02/09/02", "12345"], "james": ["James", "04/05/00", "8765"], "sarah": ["Sarah", "04/12/00", "6444"]}
-
-json_dump_users = json.dumps(users)
-json_object_users = json.loads(json_dump_users)

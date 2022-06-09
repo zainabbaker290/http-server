3e59562cee161b40cfcdb7446da4a15b66830295
zainabbaker290 <119340513@umail.ucc.ie>
2022-06-09 17:49:19 +0100

    only printing status code

--- request_users.py
+++ request_users.py
@@ -2,10 +2,5 @@ import requests
 
 response = requests.get("http://127.0.0.1:5000/users")
 
-if response:
-    print('Success! ' + str(response.status_code) )
-else:
-    print('An error has occurred.')
-
+print(response.status_code)
 print(response.text)
-

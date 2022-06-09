dfa06ed8ccd0a7099705bc43e0602fa3bc072e34
zainabbaker290 <81811035+zainabbaker290@users.noreply.github.com>
2022-06-09 08:40:58 +0100

    Fixed get endpoints (#1)
    * first two endpoints
    
    * adding third end point, doesnt work
    
    * fixed the endpoints
    
    * fixed with dumps
    
    * took out hello world

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,25 @@
 from flask import Flask
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-        return "Hello World!"
+@app.route("/users", methods=["GET"])
+def all_users():
+    #open python file and read it
+    users_file = open("users.json", "r")
+    #using json loads to read from a string 
+    users_object = json.load(users_file)
+    return json.dumps(users_object)
+
+@app.route("/users/<user_id>", methods=["GET"])
+def get_user(user_id):
+    users_file = open("users.json", "r")
+    users_object = json.load(users_file)
+
+    #using dictionary attributes to access key and values 
+    for key in users_object.keys():
+        if key == user_id:
+            return json.dumps(users_object[key])
 
 if __name__ == "__main__":
     app.run()

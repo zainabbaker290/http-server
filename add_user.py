ee44d7cdba098fc2a7fbf0d0baad48b4485ad633
zainabbaker290 <81811035+zainabbaker290@users.noreply.github.com>
2022-06-10 11:48:45 +0100

    endpoint is currently not working (#3)

---
+++ add_user.py
@@ -0,0 +1,6 @@
+import requests
+
+user_id = input("user_id: ")
+user_id = {"user_id" : str(user_id)}
+
+requests.post('http://127.0.0.1:5000/add_user/post', json=user_id)
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from pickle import TRUE
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -22,23 +23,14 @@ def get_user(user_id):
         if key == user_id:
             return json.dumps(users_object[key])
 
-@app.route("/users", methods=["GET"])
-def all_users():
-    #open python file and read it
-    users_file = open("users.json", "r")
-    #using json loads to read from a string 
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    users_file = open("users.json", "a")
     users_object = json.load(users_file)
+    data = request.json
+    users_object.write(data)
+
     return json.dumps(users_object)
-
-@app.route("/users/<user_id>", methods=["GET"])
-def get_user(user_id):
-    users_file = open("users.json", "r")
-    users_object = json.load(users_file)
-
-    #using dictionary attributes to access key and values 
-    for key in users_object.keys():
-        if key == user_id:
-            return json.dumps(users_object[key])
 
 if __name__ == "__main__":
     app.run()

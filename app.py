9e6368806c7942b6ba21d3531f29fa86e3d6461d
zainabbaker290 <81811035+zainabbaker290@users.noreply.github.com>
2022-06-11 20:44:15 +0100

    Fixing json (#4)
    * keep getting 404 error for second end point
    
    * the request is sending wrong
    
    * fixed the url
    
    * small fixes

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from pickle import TRUE
 from flask import Flask, request
 import json
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "this-is-my-secret-key"
 
 @app.route("/users", methods=["GET"])
 def all_users():
@@ -11,17 +9,16 @@ def all_users():
     users_file = open("users.json", "r")
     #using json loads to read from a string 
     users_object = json.load(users_file)
-    return json.dumps(users_object)
+    return json.dumps(users_object["friends"])
 
 @app.route("/users/<user_id>", methods=["GET"])
 def get_user(user_id):
     users_file = open("users.json", "r")
     users_object = json.load(users_file)
-
-    #using dictionary attributes to access key and values 
-    for key in users_object.keys():
-        if key == user_id:
-            return json.dumps(users_object[key])
+    list_of_users = users_object["friends"]
+    for user in list_of_users:
+        if user["user_id"] == str(user_id):
+            return json.dumps(user)
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
--- request_users_id.py
+++ request_users_id.py
@@ -1,8 +1,8 @@
 import requests
 
-user_id = input("enter user you want to search for ")
+user_id = input("enter user id:")
 
-response = requests.get("http://127.0.0.1:5000/users/", params =  str(user_id))
+response = requests.get("http://127.0.0.1:5000/users/" + str(user_id))
 
 print(response.status_code)
 print(response.text)

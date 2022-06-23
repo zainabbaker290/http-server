da7a8f60c94b88e68e486b16c30cb35b9ce82c69
zainabbaker290 <81811035+zainabbaker290@users.noreply.github.com>
2022-06-23 13:02:43 +0100

    Redoing post (#5)
    * none when appended to list
    
    * still prints none
    
    * turns out was printing result of append
    
    * how do i get the data from the request?
    
    * adds whole file at end of file
    
    * cannot get it to overwrite
    
    * post works now and files closed
    
    * added response
    
    * one endpoint

--- add_user.py
+++ add_user.py
@@ -1,6 +1,10 @@
 import requests
 
 user_id = input("user_id: ")
-user_id = {"user_id" : str(user_id)}
+name = input("name: ")
+dob = input("dob: ")
+new_user = {"user_id": str(user_id), "name": str(name), "dob": str(dob)}
+user_id = requests.post('http://127.0.0.1:5000/users', json= new_user)
 
-requests.post('http://127.0.0.1:5000/add_user/post', json=user_id)
+print(user_id.status_code)
+print(user_id.text)
--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
 
 app = Flask(__name__)
 
-@app.route("/users", methods=["GET"])
+@app.route("/users", methods=["GET", "POST"])
 def all_users():
-    #open python file and read it
-    users_file = open("users.json", "r")
-    #using json loads to read from a string 
-    users_object = json.load(users_file)
-    return json.dumps(users_object["friends"])
+    if request.method == "GET":
+        #open python file and read it
+        users_file = open("users.json", "r")
+        #using json loads to read from a string 
+        users_object = json.load(users_file)
+        users_file.close()
+        return json.dumps(users_object["friends"])
+    
+    if request.method == "POST":
+        users_file = open("users.json", "r")
+        users_object = json.load(users_file)
+        data = request.json
+        users_object["friends"].append(data)
+        users_file.close()
+        users_file = open("users.json", "w")
+        users_file.write(json.dumps(users_object))
+        users_file.close()
+        return Response(json.dumps(data), status=201)
 
 @app.route("/users/<user_id>", methods=["GET"])
 def get_user(user_id):
@@ -18,16 +31,8 @@ def get_user(user_id):
     list_of_users = users_object["friends"]
     for user in list_of_users:
         if user["user_id"] == str(user_id):
+            users_file.close()
             return json.dumps(user)
-
-@app.route("/add_user", methods=["POST"])
-def add_user():
-    users_file = open("users.json", "a")
-    users_object = json.load(users_file)
-    data = request.json
-    users_object.write(data)
-
-    return json.dumps(users_object)
 
 if __name__ == "__main__":
     app.run()
